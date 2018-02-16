ae3764e62255f3904dd4dccea54a69a2463d04b9
Derek Anderson <public@kered.org>
2018-02-16 08:40:12 -0800

    closes #7

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
