86e8953d19a312e8cdc05351a82e6b50ae60ba7d
Derek Anderson <public@kered.org>
2018-02-27 17:32:59 -0800

    -

--- gnomecast.py
+++ gnomecast.py
@@ -43,7 +43,7 @@ Thanks! - Gnomecast
   print(ERROR_MESSAGE.format(line,line))
   sys.exit(1)
 
-__version__ = '0.2.15'
+__version__ = '0.2.16'
 
 if DEPS_MET:
   pycaption.WebVTTWriter._encode = lambda self, s: s
